5e6cbc44230eec9682e2d5456702a56ac8f59572
Budschie <jul.kp06@gmail.com>
2022-01-03 20:41:02 +0100

    Removed unnecessary imports and logging outputs

--- src/main/java/com/tyler/forgezoom/ForgeZoom.java
+++ src/main/java/com/tyler/forgezoom/ForgeZoom.java
@@ -1,12 +1,12 @@
 package com.tyler.forgezoom;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod("forgezoom")
 public class ForgeZoom {
@@ -19,7 +19,7 @@ public class ForgeZoom {
 	
 	private void doClientStuff(final FMLClientSetupEvent event) {
 		Keybinds.registerKeys();
-		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+		// LOGGER.info("Got game settings {}", Minecraft.getInstance().options);
 		MinecraftForge.EVENT_BUS.register(new Keybinds());
 		MinecraftForge.EVENT_BUS.register(new ZoomHandler());
 	}
